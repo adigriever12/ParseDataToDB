c10b2d84b0903b7c25d636371af496ef8f866f4e
adigriever <adigriever@walla.com>
2016-05-07 20:55:20 +0300

    fix

---
+++ ParseData/DataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace ParseData
+{
+
+
+    partial class DataSet1
+    {
+    }
+}
+
+namespace ParseData.DataSet1TableAdapters {
+    
+    
+    public partial class Groupun_RestuarantTableAdapter {
+    }
+}
